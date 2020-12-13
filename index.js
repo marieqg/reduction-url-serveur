6869d5e002fa38b1d2a4d512f1b085420779cb83
marieqg <mariequittelier@gmail.com>
2020-12-13 21:43:09 +1300

    code clean and update

--- index.js
+++ index.js
@@ -1,55 +1,52 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
-const uid2 = require("uid2");
-const cors = require("cors");
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const uid2 = require('uid2');
+const cors = require('cors');
 
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/url", {
-  useNewUrlParser: true
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/url', {
+  useNewUrlParser: true,
 });
 
-const Address = mongoose.model("Address", {
+const Address = mongoose.model('Address', {
   longUrl: {
     type: String,
     unique: true,
-    required: true
+    required: true,
   },
   shortUrl: {
-    type: String
+    type: String,
   },
   counter: {
-    type: Number
+    type: Number,
   },
   keyUrl: {
-    type: String
-  }
+    type: String,
+  },
 });
 
 // CREATE
 
-app.post("/create", async (req, res) => {
+app.post('/create', async (req, res) => {
   try {
     // condition 1, creation uniquement si addresse envoyée en paramètre
 
     if (req.body.url) {
-      for (let i = 0; i < req.body.length; i++) {
-        console.log("hello");
-      }
       const randomStr = uid2(5);
       const newAddress = new Address({
         longUrl: req.body.url,
         shortUrl: `https://short-url-marie-quittelier.herokuapp.com/${randomStr}`,
         counter: 0,
-        keyUrl: randomStr
+        keyUrl: randomStr,
       });
       await newAddress.save();
-      return res.json({ message: "Success!" });
+      return res.json({ message: 'Success!' });
     } else {
-      return res.status(400).json({ message: "Missing parameter" });
+      return res.status(400).json({ message: 'Missing parameter' });
     }
   } catch (error) {
     return res.status(400).json({ error: { message: error } });
@@ -58,7 +55,7 @@ app.post("/create", async (req, res) => {
 
 // READ
 
-app.get("/", async (req, res) => {
+app.get('/', async (req, res) => {
   try {
     const addresses = await Address.find();
     return res.json(addresses);
@@ -69,7 +66,7 @@ app.get("/", async (req, res) => {
 
 // READ & REDIRECT
 
-app.get("/:keyUrl", async (req, res) => {
+app.get('/:keyUrl', async (req, res) => {
   try {
     const address = await Address.findOne({ keyUrl: req.params.keyUrl });
     if (address) {
@@ -88,15 +85,15 @@ app.get("/:keyUrl", async (req, res) => {
 
 // UPDATE
 
-app.post("/update", async (req, res) => {
+app.post('/update', async (req, res) => {
   try {
     const address = await Address.findById(req.body.id);
     if (address) {
       address.counter += 1;
       await address.save();
-      return res.json({ message: "Success!" });
+      return res.json({ message: 'Success!' });
     } else {
-      return res.status(400).json({ message: "Missing parameter" });
+      return res.status(400).json({ message: 'Missing parameter' });
     }
   } catch (error) {
     return res.status(400).json({ error: error.message });
@@ -104,5 +101,5 @@ app.post("/update", async (req, res) => {
 });
 
 app.listen(process.env.PORT || 3001, () => {
-  console.log("Server started");
+  console.log('Server started');
 });
